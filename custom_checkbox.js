f48c8e1a5bc110e47720cea12f1a1e085c21571e
Georg Apitz <georg@usebar.org>
2011-10-14 08:56:21 -0600

    format changes

--- custom_checkbox.js
+++ custom_checkbox.js
@@ -1,18 +1,18 @@
 (function($) {
-$.fn.styleCheckbox = function() {
-  var $wrapper = $(this),
+  $.fn.styleCheckbox = function() {
+    var $wrapper = $(this),
     $el = $wrapper.find('input.styled_checkbox');
 
-  $el.live('click', function(e) {
-    var $currentCheckbox = $(e.currentTarget);
-    if ($currentCheckbox.attr("disabled", false)) {
-      var $currentWrapper = $currentCheckbox.parents('.styled_checkbox_wrap');
-      if ($currentCheckbox.is(':checked')) {
-        $currentWrapper.addClass('wrap_checked');
-      } else {
-        $currentWrapper.removeClass('wrap_checked');
+    $el.live('click', function(e) {
+      var $currentCheckbox = $(e.currentTarget);
+      if ($currentCheckbox.attr("disabled", false)) {
+        var $currentWrapper = $currentCheckbox.parents('.styled_checkbox_wrap');
+        if ($currentCheckbox.is(':checked')) {
+          $currentWrapper.addClass('wrap_checked');
+        } else {
+          $currentWrapper.removeClass('wrap_checked');
+        }
       }
-    }
-  });
-}
+    });
+  }
 })(jQuery);
