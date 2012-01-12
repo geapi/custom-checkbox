a76306632d6eb5ee6c962bd0dce9f89b1436e4f6
Georg Apitz <georg@usebar.org>
2012-01-11 21:46:10 -0700

    fix issue where checked checkboxes would not be checked on initial load
    updated example to have a checked and an unchecked box

--- custom_checkbox.js
+++ custom_checkbox.js
@@ -2,10 +2,18 @@
   $.fn.styleCheckbox = function() {
     var $wrapper = $(this),
     $el = $wrapper.find('input.styled_checkbox');
+	// check the ones that are set to check on initialize
+	// TODO should move into it's own function
+	$el.each(function(index){
+		var $checkBox = $($el[index]);
+		if ($checkBox.is(':checked') ){
+			$checkBox.parents('.styled_checkbox_wrap').addClass('wrap_checked');
+		}
+	});
 
     $el.live('click', function(e) {
       var $currentCheckbox = $(e.currentTarget);
-      if (!$currentCheckbox.attr("disabled") {
+      if (!$currentCheckbox.attr("disabled")) {
         var $currentWrapper = $currentCheckbox.parents('.styled_checkbox_wrap');
         if ($currentCheckbox.is(':checked')) {
           $currentWrapper.addClass('wrap_checked');
