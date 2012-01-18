4dd488a22d5bf90da64b0716bc6226ba13f93a15
Georg Apitz <georg@usebar.org>
2012-01-17 21:35:18 -0700

    Finished jasmine tests.

--- spec/jquery.custom_checkbox.spec.js
+++ spec/jquery.custom_checkbox.spec.js
@@ -1,41 +1,52 @@
 describe("custom_checkbox", function() {
-	var checkedCheckbox, 
-		unCheckedCheckbox;
-	
+	var checkedCheckbox,
+			unCheckedCheckbox,
+			disabledCheckbox;
+
 	beforeEach(function() {
 		jasmine.getFixtures().set(
-			'<div class="styled_checkbox_wrap"><input type="checkbox" name="test" value="1" id="unchecked" class="styled_checkbox"/> </div> <label for="test">check me</label>' +
-			'<div class="styled_checkbox_wrap"> <input type="checkbox" name="test_1" value="1" id="checked" class="styled_checkbox" checked="checked"/></div> <label for="test_1">uncheck me</label>' +
-			'<div class="styled_checkbox_wrap"> <input type="checkbox" name="test_2" value="1" id="disabled" class="styled_checkbox" disabled="disabled"/></div> <label for="test_1">uncheck me</label>'
+				'<div class="styled_checkbox_wrap"><input type="checkbox" name="test" value="1" id="unchecked" class="styled_checkbox"/> </div> <label for="test">check me</label>' +
+				'<div class="styled_checkbox_wrap"> <input type="checkbox" name="test_1" value="1" id="checked" class="styled_checkbox" checked="checked"/></div> <label for="test_1">uncheck me</label>' +
+				'<div class="styled_checkbox_wrap"> <input type="checkbox" name="test_2" value="1" id="disabled" class="styled_checkbox" disabled="disabled"/></div> <label for="test_1">uncheck me</label>'
 		);
 		$('.styled_checkbox_wrap').styleCheckbox();
 		checkedCheckbox = $('#checked');
 		uncheckedCheckbox = $('#unchecked');
 		disabledCheckbox = $('#disabled');
 	});
-	
+
 	describe("initial state", function() {
 		it("should set the correct style on non checked checkboxes", function() {
 			expect(uncheckedCheckbox.parent().hasClass("wrap_checked")).toBeFalsy();
 		});
-		
+
 		it("should set the correct state on checked checkboxes", function() {
 			expect(checkedCheckbox.parent().hasClass("wrap_checked")).toBeTruthy();
 		});
-		
+
 		it("should handle disabled checkboxes", function() {
 			expect(disabledCheckbox.attr("disabled") == "disabled").toBeTruthy();
 		});
 	});
-	
+
 	describe("clicking on the checkbox", function() {
 		it("should uncheck a checked checkbox", function() {
+			$parent = $(".styled_checkbox_wrap #checked");
+			spyOn(jQuery.fn, "parents").andReturn($parent);
 
+			var removeClassSpy = spyOn($parent, "removeClass");
+			uncheckedCheckbox.trigger("click");
+			expect(removeClassSpy).toHaveBeenCalledWith("wrap_checked");
 		});
-		
+
 		it("should check an uncheck checkbox", function() {
+			$parent = $(".styled_checkbox_wrap #unchecked");
+			spyOn(jQuery.fn, "parents").andReturn($parent);
 
+			var addClassSpy = spyOn($parent, "addClass");
+			checkedCheckbox.trigger("click");
+			expect(addClassSpy).toHaveBeenCalledWith("wrap_checked");
 		});
-		
+
 	});
 });
--- src/custom_checkbox.js
+++ src/custom_checkbox.js
@@ -1,26 +1,26 @@
 (function($) {
-  $.fn.styleCheckbox = function() {
-    var $wrapper = $(this),
-    $el = $wrapper.find('input.styled_checkbox');
-	// check the ones that are set to check on initialize
-	// TODO should move into it's own function
-	$el.each(function(index){
-		var $checkBox = $($el[index]);
-		if ($checkBox.is(':checked') ){
-			$checkBox.parents('.styled_checkbox_wrap').addClass('wrap_checked');
-		}
-	});
+	$.fn.styleCheckbox = function() {
+		var $wrapper = $(this),
+				$el = $wrapper.find('input.styled_checkbox');
+		// check the ones that are set to check on initialize
+		// TODO should move into it's own function
+		$el.each(function(index) {
+			var $checkBox = $($el[index]);
+			if ($checkBox.is(':checked')) {
+				$checkBox.parents('.styled_checkbox_wrap').addClass('wrap_checked');
+			}
+		});
 
-    $el.live('click', function(e) {
-      var $currentCheckbox = $(e.currentTarget);
-      if (!$currentCheckbox.attr("disabled")) {
-        var $currentWrapper = $currentCheckbox.parents('.styled_checkbox_wrap');
-        if ($currentCheckbox.is(':checked')) {
-          $currentWrapper.addClass('wrap_checked');
-        } else {
-          $currentWrapper.removeClass('wrap_checked');
-        }
-      }
-    });
-  }
+		$el.live('click', function(e) {
+			var $currentCheckbox = $(e.currentTarget);
+			if (!$currentCheckbox.attr("disabled")) {
+				var $currentWrapper = $currentCheckbox.parents('.styled_checkbox_wrap');
+				if ($currentCheckbox.is(':checked')) {
+					$currentWrapper.addClass('wrap_checked');
+				} else {
+					$currentWrapper.removeClass('wrap_checked');
+				}
+			}
+		});
+	}
 })(jQuery);
